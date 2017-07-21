6f07fd5a9ebae591f2761df1b5a03035cc3c9be4
Lawrence Valiquette <lawrence.valiquette@omniresources.com>
2017-07-21 15:31:13 -0500

    Added charity standings, currently charities are hardcoded in the component.ts

--- src/app/cards/charity-standings/charity-standings.component.ts
+++ src/app/cards/charity-standings/charity-standings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BlockchainService } from '../../services/blockchain.service';
+import { Charity } from '../../../models/Charity';
 
 @Component({
   selector: 'app-charity-standings',
@@ -10,11 +11,54 @@ export class CharityStandingsComponent implements OnInit {
   blockchainService: BlockchainService;
   balances: Map<string, number>;
 
+  charity1 : Charity; 
+  charity2 : Charity; 
+  charity3 : Charity; 
+  charities: Charity[] = [];
+  totalCharityAmount: number;
+  
   constructor(private _blockchainService: BlockchainService) {
     this.blockchainService = _blockchainService;
-  }
+
+    
+    //test data 
+      this.charity1 = {
+      id: 1,
+      name: "animals",
+      amount: 100,
+      backers: 5,
+      icon: "pets"
+    }  
+    this.charities.push(this.charity1);
+
+    this.charity2 = {
+      id: 2,
+      name: "kids",
+      amount: 25,
+      backers: 4,
+      icon: "child_care"
+    }   
+    this.charities.push(this.charity2);
+
+    this.charity3 = {
+      id: 3,
+      name: "disaster",
+      amount: 50,
+      backers: 7,
+      icon: "warning"
+    }  
+    this.charities.push(this.charity3);
+
+    this.charities.sort(function (a,b){
+      return b.amount - a.amount;
+    });
+
+    this.totalCharityAmount = this.charities.reduce(function (sum, value){
+      return sum + value.amount;
+    }, 0);
+   }
 
   ngOnInit() {
-
   }
+
 }
