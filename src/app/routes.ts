e0bc0e24e5b000097861e8b46871e2fbf1770cb0
Kevin Brey <manbearwiz@gmail.com>
2017-07-27 10:36:07 -0500

    feat: add standings page

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,12 @@
 import { HomepageComponent } from './pages/homepage';
 import { AboutComponent } from './pages/about';
+import { StandingsComponent } from './pages/standings';
 
 import { Routes } from '@angular/router';
 
 export const ROCK_CHAIN_ROUTES: Routes = [
     { path: '', component: HomepageComponent, pathMatch: 'full' },
     { path: 'about', component: AboutComponent, pathMatch: 'full' },
+    { path: 'standings', component: StandingsComponent, pathMatch: 'full' },
     { path: '**', redirectTo: '' },
 ];
