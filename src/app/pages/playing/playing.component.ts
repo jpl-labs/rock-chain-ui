8ced6baee41aa63c8d280759e528137bb6aa9090
Kevin Brey <manbearwiz@gmail.com>
2017-07-31 16:35:03 -0500

    feat(now-playing): load last song on init

--- src/app/pages/playing/playing.component.ts
+++ src/app/pages/playing/playing.component.ts
@@ -1,25 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AudioSong } from '../../../models/PlayerStatus';
 import { BlockchainService } from '../../services/blockchain.service';
 import { WagerService } from '../../services/wager.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-playing',
   templateUrl: './playing.component.html',
   styleUrls: ['./playing.component.css']
 })
-export class PlayingComponent implements OnInit {
-
+export class PlayingComponent implements OnInit, OnDestroy {
 
   currentSong: AudioSong;
+
+  private subscriptions: Array<Subscription> = [];
 
   constructor(private wagerService: WagerService, private blockchainService: BlockchainService) { }
 
   ngOnInit() {
-    const songSub = this.blockchainService.getSongChangedEmitter()
+
+    this.subscriptions.push(this.wagerService.getLastSong()
+      .subscribe(result => {
+        const jsonAscii = this.blockchainService.web3.toAscii(result.match(new RegExp('7b22.+227d'))[0]);
+        const songData = JSON.parse(jsonAscii);
+        this.currentSong = songData;
+      }));
+
+    this.subscriptions.push(this.blockchainService.getSongChangedEmitter()
       .filter(result => result.to === this.wagerService.instance.address
         && result.from === this.blockchainService.web3.eth.accounts[0])
-      .subscribe(result => this.currentSong = this.parseSongHex(result.input));
+      .subscribe(result => this.currentSong = this.parseSongHex(result.input)));
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
   }
 
   parseSongHex = (hexString: string): AudioSong => {
